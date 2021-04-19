9d33cefe4532422d7070713bfa2ad221f6e78274
tiffanyharris711 <tiffanyaharris711@gmail.com>
2021-04-18 20:46:43 -0500

    working copy d3 javascript file

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,69 +1,82 @@
-d3.csv('assets/data/data.csv', function (data) {
-    // Variables
-    var body = d3.select('body')
-      var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-      var h = 500 - margin.top - margin.bottom
-      var w = 500 - margin.left - margin.right
+// Variables    
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+}
+
+svgW = 960;
+svgH = 500;
+
+var h = svgH - margin.top - margin.bottom
+var w = svgW - margin.left - margin.right
+
+// Scatter, svg
+var scatter = d3.select('#scatter')
+  .append("svg")
+  .attr("width", svgW)
+  .attr("height", svgH);
+
+// Group
+var group = scatter.append("g")
+  .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
+
+
+d3.csv('assets/data/data.csv').then(function (data) {
+    
+    data.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
       // Scales
-    var colorScale = d3.scale.category20()
-    var xScale = d3.scale.linear()
-    //   .domain([
-    //       d3.min([0,d3.min(data,function (d) { return d.poverty })]),
-    //       d3.max([0,d3.max(data,function (d) { return d.poverty })])
-    //       ])
-      .domain([0,25])
+    var xScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.poverty))
       .range([0,w])
-    var yScale = d3.scale.linear()
-    //   .domain([
-    //       d3.min([0,d3.min(data,function (d) { return d.healthcare })]),
-    //       d3.max([0,d3.max(data,function (d) { return d.healthcare })])
-    //       ])
-      .domain([0,25])
-      .range([h,0])
-      // SVG
-      var svg = body.append('svg')
-          .attr('height',h + margin.top + margin.bottom)
-          .attr('width',w + margin.left + margin.right)
-        .append('g')
-          .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
-      // X-axis
-      var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .ticks(2)
-        .orient('bottom')
-    // Y-axis
-      var yAxis = d3.svg.axis()
-        .scale(yScale)
-        .ticks(2)
-        .orient('left')
-    // Circles
-    var circles = svg.selectAll('circle')
-        .data(data)
-        .enter()
-      .append('circle')
-        .attr('cx',function (d) { return xScale(d.healthcare) })
-        .attr('cy',function (d) { return yScale(d.poverty) })
-        .attr('r','10')
-        .attr('fill',function (d,i) { return colorScale(i) })
-        .on('mouseover', function () {
-          d3.select(this)
-            .transition()
-            .duration(500)
-            .attr('r',20)
-        })
-        .on('mouseout', function () {
-          d3.select(this)
-            .transition()
-            .duration(400)
-            .attr('r',10)
-        })
-      .append('title') // Tooltip
-        .text(function (d) { return d.state +
-                             '\nHealthcare: ' + d.poverty +
-                             '\nPoverty: ' + d.healthcare })
+      // .nice();
+
+    var yScale = d3.scaleLinear()
+      .domain([6,d3.max(data, d => d.healthcare)])
+      .range([h,0]);
+
     // X-axis
-    svg.append('g')
-        .attr('class','axis')
+    var xAxis = d3.axisBottom(xScale);
+        
+    // Y-axis
+      var yAxis = d3.axisLeft(yScale);
+      
+    group.append("g").attr("transform", `translate(0, ${h})`).call(xAxis);
+    group.append("g").call(yAxis);
+
+    // Circles
+    var circles = group.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx',d => xScale(d.healthcare))
+      .attr('cy',d => yScale(d.poverty))
+      .attr('r','10')
+      .attr('fill','lightblue')
+      .classed("stateCircle",true)
+      .attr("opacity", 0.75);
+      // .on('mouseover', function () {
+      //   d3.select(this)
+      //     .transition()
+      //     .duration(500)
+      //     .attr('r',20)
+      // })
+      // .on('mouseout', function () {
+      //   d3.select(this)
+      //     .transition()
+      //     .duration(400)
+      //     .attr('r',10)
+      // })
+    // .append('title') // Tooltip
+    //   .text(function (d) { return d.state +
+    //                         '\nHealthcare: ' + d.poverty +
+    //                         '\nPoverty: ' + d.healthcare })
+    // X-axis
+    group.attr('class','axis')
         .attr('transform', 'translate(0,' + h + ')')
         .call(xAxis)
       .append('text') // X-axis Label
@@ -74,8 +87,7 @@ d3.csv('assets/data/data.csv', function (data) {
         // .style('text-anchor','center')
         .text('In Poverty (%)')
     // Y-axis
-    svg.append('g')
-        .attr('class', 'axis')
+    group.attr('class', 'axis')
         .call(yAxis)
       .append('text') // y-axis Label
         .attr('class','label')
